aa82c8dc7ca281c3e43d1a44de5a4c6644d62b6e
cecto01 <cecto01@ca.com>
2017-03-30 18:22:12 +0200

    EarthquakeAdapter

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -6,6 +6,37 @@ package com.example.android.quakereport;
 
 public class Earthquake {
 
-    public Earthquake() {
+    private String mMagnitude;
+    private String mLocation;
+    private String mDate;
+
+    public Earthquake(String mMagnitude, String mLocation, String mDate) {
+        this.mMagnitude = mMagnitude;
+        this.mLocation = mLocation;
+        this.mDate = mDate;
+    }
+
+    public String getmMagnitude() {
+        return mMagnitude;
+    }
+
+    public void setmMagnitude(String mMagnitude) {
+        this.mMagnitude = mMagnitude;
+    }
+
+    public String getmLocation() {
+        return mLocation;
+    }
+
+    public void setmLocation(String mLocation) {
+        this.mLocation = mLocation;
+    }
+
+    public String getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(String mDate) {
+        this.mDate = mDate;
     }
 }
